244920425677cbca7a11d806616076c635e4694c
dbls37100 <dylan.belouis@gmail.com>
2022-06-09 21:53:54 +0200

    add ourRoom section with description table

--- components/OurRoom.js
+++ components/OurRoom.js
@@ -1,11 +1,117 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 import styles from "../styles/components/OurRoom.module.css";
 
-const OurRoom = () => {
+export default function BasicTable() {
   return (
-    <div className={styles.OurRoom} id="OurRoom">
-      <h2>Nos Salles</h2>
+    <div className={styles.OurRoom}>
+      <h5 className={styles.OurRoom_h5_title}>Salles & espaces</h5>
+      <TableContainer
+        component={Paper}
+        className={styles.OurRoom_TableContainer}
+      >
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Espace</TableCell>
+              <TableCell align="right">Superficie</TableCell>
+              <TableCell align="right">Cocktail</TableCell>
+              <TableCell align="right">Conférence</TableCell>
+              <TableCell align="right">Banquet</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                Salle François 1er
+              </TableCell>
+              <TableCell align="right">500 m²</TableCell>
+              <TableCell align="right">500</TableCell>
+              <TableCell align="right">300</TableCell>
+              <TableCell align="right">450</TableCell>
+            </TableRow>
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                Salle Léonard de Vinci
+              </TableCell>
+              <TableCell align="right">300 m²</TableCell>
+              <TableCell align="right">300</TableCell>
+              <TableCell align="right">150</TableCell>
+              <TableCell align="right">200</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <h6>Espaces groupés</h6>
+      <TableContainer
+        component={Paper}
+        className={styles.OurRoom_TableContainer}
+      >
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Espace</TableCell>
+              <TableCell align="right">Superficie</TableCell>
+              <TableCell align="right">Cocktail</TableCell>
+              <TableCell align="right">Conférence</TableCell>
+              <TableCell align="right">Banquet</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow
+            // sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell scope="row">
+                Salle François 1er + Caveau + Bar
+              </TableCell>
+              <TableCell align="right">650 m²</TableCell>
+              <TableCell align="right">650</TableCell>
+              <TableCell align="right">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </TableCell>
+              <TableCell align="right">600</TableCell>
+            </TableRow>
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell>
+                Salle François 1er + Caveau + Bar + Choiseul
+              </TableCell>
+              <TableCell align="right">1000 m²</TableCell>
+              <TableCell align="right">750</TableCell>
+              <TableCell align="right">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </TableCell>
+              <TableCell align="right">700</TableCell>
+            </TableRow>
+            <TableRow
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                L'Ensemble des Salles
+              </TableCell>
+              <TableCell align="right">1000 m²</TableCell>
+              <TableCell align="right">1000</TableCell>
+              <TableCell align="right">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </TableCell>
+              <TableCell align="right">1000</TableCell>
+              {/* <TableCell align="right">200</TableCell> */}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
-};
-
-export default OurRoom;
+}
