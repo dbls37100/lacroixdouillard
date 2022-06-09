cabc59f90b934236024569d4d10d96088ebdae0f
dbls37100 <dylan.belouis@gmail.com>
2022-06-09 22:07:16 +0200

    fix deploy problem

--- components/OurRoom.js
+++ components/OurRoom.js
@@ -6,7 +6,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
 import styles from "../styles/components/OurRoom.module.css";
 
 export default function BasicTable() {
---
+++ components/Quotation.js
@@ -0,0 +1,11 @@
+import styles from "../styles/components/Quotation.module.css";
+
+const Quotation = () => {
+  return (
+    <div>
+      <h1>Quotation</h1>
+    </div>
+  );
+};
+
+export default Quotation;
