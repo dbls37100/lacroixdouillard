a8f3cee28e5a64d7130526221b4a259ef01a1528
dbls37100 <dylan.belouis@gmail.com>
2022-06-27 16:00:22 +0200

    add responsives rules for map

--- components/Location.js
+++ components/Location.js
@@ -9,11 +9,20 @@ const Location = () => {
         <h5>Adresse</h5>
         <p>510 Quai des Violettes, 37400 Amboise</p>
       </div>
-      <iframe
-        width="500"
-        height="300"
-        src="https://api.maptiler.com/maps/c795e896-f66d-42ab-a214-abfcd5d06276/?key=BMUKDpp6B2okBlMX42LJ#15.6/47.42265/1.00550"
-      ></iframe>
+      <div className={styles.Location_iFrame_mobile}>
+        <iframe
+          width="350"
+          height="300"
+          src="https://api.maptiler.com/maps/c795e896-f66d-42ab-a214-abfcd5d06276/?key=BMUKDpp6B2okBlMX42LJ#15.6/47.42265/1.00550"
+        ></iframe>
+      </div>
+      <div className={styles.Location_iFrame_desktop}>
+        <iframe
+          width="800"
+          height="500"
+          src="https://api.maptiler.com/maps/c795e896-f66d-42ab-a214-abfcd5d06276/?key=BMUKDpp6B2okBlMX42LJ#15.6/47.42265/1.00550"
+        ></iframe>
+      </div>
     </div>
   );
 };
