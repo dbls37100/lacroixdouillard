4c465ecd8871e5f36fce3343b4c5e6af54ce730c
dbls37100 <dylan.belouis@gmail.com>
2022-07-06 13:34:41 +0200

    change h1 content

--- components/Banner.js
+++ components/Banner.js
@@ -6,9 +6,7 @@ const Banner = () => {
     <section className={styles.Banner}>
       <div className={styles.Banner_div_text_Invisible}></div>
       <div className={styles.Banner_div_text}>
-        <h1 className={styles.Banner_text}>
-          Bienvenue aux Caves de la Croix Douillard
-        </h1>
+        <h1 className={styles.Banner_text}>Les Caves de la Croix Douillard</h1>
         <p className={styles.Banner_text1}>
           La Cave de la Croix Douillard vous accueille dans sa salle de
           réception au décor atypique et soigné pour organiser tous vos
