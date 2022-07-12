1e0ff97ae7eea4fa810e53e6281f7a3b3e9f304a
dbls37100 <dylan.belouis@gmail.com>
2022-07-12 19:22:35 +0200

    change google verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,6 @@ export default function Document() {
     <Html lang="fr">
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="google-site-verification"
-          content="ca7u7GMXe6n3aqnzsFOyf9iwVOHG6NAMCHPZVeut2bI"
-        />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Alex+Brush&display=swap"
